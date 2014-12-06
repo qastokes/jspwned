ac4776e4416bdf1c55063cd945f5bc80c0c05d2f
johnataylor <johtaylo@microsoft.com>
2014-12-05 21:44:04 -0800

    func

--- program.js
+++ program.js
@@ -3,4 +3,10 @@ document.writeln('and goodnight');
 
 // writing to the console works like this (its easy) :
 
+myfunc = function (x, y) {
+  return x + y;
+} 
+
 console.log('hello from the console');
+
+console.log(myfunc(3, 4));
