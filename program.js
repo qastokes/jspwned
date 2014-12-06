dca77afbd2ad368901c107cf2e0b79db1d15527c
qastokes <qastokes@gmail.com>
2014-12-05 21:52:49 -0800

    Update program.js

--- program.js
+++ program.js
@@ -1,12 +1,10 @@
 document.writeln('hello, world!');
 document.writeln('and goodnight');
 
-// writing to the console works like this (its easy) :
-
 myfunc = function (x, y) {
   return x + y;
 } 
-
+// writing to the console works like this (its easy) :
 console.log('hello from the console');
 
 console.log(myfunc(3, 4));
